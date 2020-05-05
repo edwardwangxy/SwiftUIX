7768acd70a447b539f8d839a21ad8b91f570f8be
Edward Wang <admin@uwwxy.com>
2020-05-05 19:58:36 -0400

    update animate

--- Sources/Intermodular/Helpers/UIKit/UIHostingTableViewController.swift
+++ Sources/Intermodular/Helpers/UIKit/UIHostingTableViewController.swift
@@ -404,7 +404,7 @@ extension UITableView {
         let sections = self.numberOfSections
         let rows = self.numberOfRows(inSection: sections - 1)
         if (rows > 0){
-            self.scrollToRow(at: NSIndexPath(row: rows - 1, section: sections - 1) as IndexPath, at: .bottom, animated: true)
+            self.scrollToRow(at: NSIndexPath(row: rows - 1, section: sections - 1) as IndexPath, at: .bottom, animated: animated)
         }
     }
 }
